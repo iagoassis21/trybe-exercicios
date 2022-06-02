af42ae75148f8995aaf1335aa983d0de6c8d5a54
Iago <iagoassis21@outlook.com>
2022-06-02 00:28:40 -0300

    Exercicios

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercises4.js
@@ -0,0 +1,39 @@
+// 4 - Escreva uma função filterPeople que, dada uma lista de pessoas, retorna todas as pessoas australianas que nasceram no século 20:
+
+const people = [
+    {
+      name: 'Nicole',
+      bornIn: 1992,
+      nationality: 'Australian',
+    },
+    {
+      name: 'Harry',
+      bornIn: 2008,
+      nationality: 'Australian',
+    },
+    {
+      name: 'Toby',
+      bornIn: 1901,
+      nationality: 'Australian',
+    },
+    {
+      name: 'Frida',
+      bornIn: 1960,
+      nationality: 'Dannish',
+    },
+    {
+      name: 'Fernando',
+      bornIn: 2001,
+      nationality: 'Brazilian',
+    },
+  ];
+  
+  // escreva filterPeople abaixo
+
+// const filterPeople = () => {
+//     return people.filter((list) => list.nationality === 'Australian' && (list.bornIn > 1900 && list.bornIn < 2002))
+// }
+
+
+
+console.log(filterPeople())
